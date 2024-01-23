d7e2b5333e1cf3d555c6b7e6ee6cd51c65b3a86f
tblagarrett <122318726+tblagarrett@users.noreply.github.com>
2024-01-22 16:21:36 -0800

    Main.js comment, rocket moves side to side

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -12,12 +12,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
     update() {
         // left/right movement
-        if (!this.isFiring) {
-            if (keyLEFT.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed
-            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
-                this.x += this.moveSpeed
-            }
+        if (keyLEFT.isDown && this.x >= borderUISize + this.width) {
+            this.x -= this.moveSpeed
+        } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+            this.x += this.moveSpeed
         }
         // fire button
         if (Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring) {
